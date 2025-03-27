3ab4aac3d75ddf0e869b63b8b2e0cc9ea6f822a1
KhiemNguyen15 <khiemsa15@gmail.com>
2025-03-27 17:14:29 -0500

    docs: Update --help info

--- img2txt/img2txt.py
+++ img2txt/img2txt.py
@@ -6,10 +6,20 @@ import click
 import pytesseract
 
 
-@click.command
+@click.command()
 @click.argument("image_path", required=False, type=click.Path(exists=True))
 def main(image_path):
-    """Extract text from an image file or from stdin and print it to stdout."""
+    """Extract text from an image file or stdin and print to stdout
+
+    Examples:
+
+    \b
+    $ img2txt image.png
+    $ cat image.png | img2txt
+
+    For help:
+    $ img2txt --help
+    """
     try:
         if image_path:
             image = Image.open(image_path)
